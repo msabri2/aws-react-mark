1dabce72f8dc6bbf0aa7224a3b9136db8adc29d2
msabri2 <mahdisabri@Mahdis-MacBook-Air.local>
2022-01-25 17:00:26 -0500

    test push 03

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header.js';
+import Main from './components/Main.js';
+import Footer from './components/Footer.js';
 
-function App() {
+class App extends React.Component {
+  render() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React in 7 days jimmy since it is great
-        </a>
-      </header>
+     <Header />
+     <Main />
+
+      <Footer />
     </div>
   );
+  }
 }
 
 export default App;
